ee3f73de398ab314b2703ce9af9a2ced90a7077a
Dominic <Dominic@gmail.com>
2018-11-06 18:02:19 -0500

    first commit

---
+++ ComicGallery/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ComicGallery
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ComicGallery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ComicGallery.MvcApplication" Language="C#" %>
